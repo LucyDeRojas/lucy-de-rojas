a4e97e9c692d762af1fc9e69780e15b80839a860
Lucy de Rojas <lucyderojas@gmail.com>
2024-11-24 21:51:17 +0000

    links in UI finished

--- src/pages/UI.js
+++ src/pages/UI.js
@@ -13,6 +13,10 @@ export default function UI () {
 
 
         let imageMarginY = "my-14 shadow-2xl rounded-xl";
+        let linkToPrototypesStyle = "p-5 my-5 rounded-xl bg-red-200 text-center";
+        let mobilePrototypeText = "Click this box to see mobile 📱 prototype.";
+        let desktopPrototypeText = "Click this box to see desktop 💻 prototype.";
+
 
 
 
@@ -32,8 +36,77 @@ From concept to completion, my work is driven by creativity, collaboration, and 
 </p>
 
 
+{/*
+water company
+mobile link:  🎈
+https://www.figma.com/proto/Fvo2PU4HmlT58ajRuWibrt/Water-Bottling-Aqua-Quacamaya?node-id=555-378&node-type=canvas&viewport=548%2C371%2C0.02&t=RmMHJiDfE8M0CXpL-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=564%3A22&show-proto-sidebar=1
+
+Desktrop Link: 🎈
+https://www.figma.com/proto/Fvo2PU4HmlT58ajRuWibrt/Water-Bottling-Aqua-Quacamaya?node-id=555-378&node-type=canvas&viewport=548%2C371%2C0.02&t=W37bH0mX43j2XuRI-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=577%3A12&show-proto-sidebar=1
 
 
+
+
+Laceys
+mobile link:
+https://www.figma.com/proto/pXc7s9wQgbpIMZBXDT6n1h/Laceys?node-id=0-1&node-type=canvas&viewport=343%2C478%2C0.02&t=vOaL0nP26bwqKR1d-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=55%3A7875&show-proto-sidebar=1
+
+desktop link:
+https://www.figma.com/proto/pXc7s9wQgbpIMZBXDT6n1h/Laceys?node-id=0-1&node-type=canvas&viewport=343%2C478%2C0.02&t=vOaL0nP26bwqKR1d-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=129%3A6902&show-proto-sidebar=1
+
+
+
+chanelle
+mobile page link:
+https://www.figma.com/proto/e1VmnoQvT9wQDNCXyTAoAk/Beauty-by-Chanelle?node-id=0-1&node-type=canvas&viewport=1351%2C-1426%2C0.27&t=B4O2DvcVb5k3ZBZi-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=223%3A152&show-proto-sidebar=1
+
+desktop page link:
+https://www.figma.com/proto/e1VmnoQvT9wQDNCXyTAoAk/Beauty-by-Chanelle?node-id=0-1&node-type=canvas&viewport=1351%2C-1426%2C0.27&t=B4O2DvcVb5k3ZBZi-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=304%3A527&show-proto-sidebar=1
+
+
+
+
+Naphill
+
+mobile link:
+https://www.figma.com/proto/tYvFzMtJQ7Mg36L9ZmXJAV/Naphill-Tennis-Club?node-id=0-1&node-type=canvas&viewport=481%2C359%2C0.03&t=mCa1Fbg0nsFraa56-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=207%3A191&show-proto-sidebar=1
+
+
+desktrop link:
+https://www.figma.com/proto/tYvFzMtJQ7Mg36L9ZmXJAV/Naphill-Tennis-Club?node-id=0-1&node-type=canvas&viewport=481%2C359%2C0.03&t=mCa1Fbg0nsFraa56-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=207%3A349&show-proto-sidebar=1
+
+
+
+
+
+
+
+
+My site:
+mobile page link:
+https://www.figma.com/proto/kmyM86eZQFUqKt3I2THPuh/Lucy-de-Rojas?node-id=0-1&node-type=canvas&viewport=669%2C182%2C0.07&t=dITXoyTdpFvC0P13-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1%3A142&show-proto-sidebar=1
+
+desktop page link:
+https://www.figma.com/proto/kmyM86eZQFUqKt3I2THPuh/Lucy-de-Rojas?node-id=0-1&node-type=canvas&viewport=669%2C182%2C0.07&t=dITXoyTdpFvC0P13-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=123%3A2622&show-proto-sidebar=1
+
+
+
+
+
+cssc centenary:
+desktop:
+https://www.figma.com/proto/MmmDVxYW4T9L5TNRozYH62/CSSC-Centenary?node-id=0-1&node-type=canvas&viewport=-3%2C68%2C0.15&t=v0pajn9PQOmTNVeq-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=11%3A4&show-proto-sidebar=1
+
+mobile:
+https://www.figma.com/proto/MmmDVxYW4T9L5TNRozYH62/CSSC-Centenary?node-id=0-1&node-type=canvas&viewport=-3%2C68%2C0.15&t=v0pajn9PQOmTNVeq-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=28%3A346&show-proto-sidebar=1
+
+
+
+
+ */}
+
+
+<Link href="https://www.figma.com/proto/Fvo2PU4HmlT58ajRuWibrt/Water-Bottling-Aqua-Quacamaya?node-id=555-378&node-type=canvas&viewport=548%2C371%2C0.02&t=W37bH0mX43j2XuRI-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=577%3A12&show-proto-sidebar=1" className={linkToPrototypesStyle} target="_blank">
 <Image
                         src="/Media/Images/Portfolio/UI Design/Water-FULLscreen3.jpg"
                         width={500}
@@ -41,7 +114,9 @@ From concept to completion, my work is driven by creativity, collaboration, and 
                         alt=""
                         layout="responsive"
                         className={imageMarginY}
-                         />
+                        />
+                        <p>{desktopPrototypeText}</p>
+                        </Link>
 
 
 
@@ -65,6 +140,7 @@ From concept to completion, my work is driven by creativity, collaboration, and 
                      className={imageMarginY}
                       />
 
+<Link href="https://www.figma.com/proto/Fvo2PU4HmlT58ajRuWibrt/Water-Bottling-Aqua-Quacamaya?node-id=555-378&node-type=canvas&viewport=548%2C371%2C0.02&t=RmMHJiDfE8M0CXpL-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=564%3A22&show-proto-sidebar=1" target="_blank" className={linkToPrototypesStyle}>
 <Image
                           src="/Media/Images/Portfolio/UI Design/Water-Mobile-screens.jpg"
                           width={500}
@@ -72,7 +148,9 @@ From concept to completion, my work is driven by creativity, collaboration, and 
                           alt=""
                           layout="responsive"
                           className={imageMarginY}
-                           />
+                          />
+<p>{mobilePrototypeText}</p>
+                          </Link>
 
 
 
@@ -97,7 +175,7 @@ From concept to completion, my work is driven by creativity, collaboration, and 
 
 
 
-
+<Link href="https://www.figma.com/proto/pXc7s9wQgbpIMZBXDT6n1h/Laceys?page-id=0%3A1&node-id=129-6902&node-type=frame&t=ARhwU9O0ZVFQfBdI-0&scaling=scale-down&content-scaling=fixed&starting-point-node-id=129%3A6902&show-proto-sidebar=1" target="_blank" className={linkToPrototypesStyle}>
 <Image
                    src="/Media/Images/Portfolio/UI Design/Laceys-Desktop-home-page.jpg"
                    width={500}
@@ -105,9 +183,15 @@ From concept to completion, my work is driven by creativity, collaboration, and 
                    alt=""
                    layout="responsive"
                    className={imageMarginY}
-                    />
+                   />
+<p>{desktopPrototypeText}</p>
+                   </Link>
 
 
+
+
+
+<Link href="https://www.figma.com/proto/pXc7s9wQgbpIMZBXDT6n1h/Laceys?page-id=0%3A1&node-id=55-7875&node-type=frame&t=ARhwU9O0ZVFQfBdI-0&scaling=scale-down&content-scaling=fixed&starting-point-node-id=55%3A7875&show-proto-sidebar=1" target="_blank" className={linkToPrototypesStyle}>
 
 <Image
                            src="/Media/Images/Portfolio/UI Design/Laceys-Mobile-Screens.jpg"
@@ -116,8 +200,9 @@ From concept to completion, my work is driven by creativity, collaboration, and 
                            alt=""
                            layout="responsive"
                            className={imageMarginY}
-                            />
-
+                           />
+<p>{mobilePrototypeText}</p>
+                           </Link>
 
 
 <Image
@@ -135,7 +220,7 @@ From concept to completion, my work is driven by creativity, collaboration, and 
 
 
 
-
+<chanelle className={linkToPrototypesStyle}>
 
 <Image
               src="/Media/Images/Portfolio/UI Design/ChanelleScreenShot.jpg"
@@ -144,14 +229,21 @@ From concept to completion, my work is driven by creativity, collaboration, and 
               alt=""
               layout="responsive"
               className={imageMarginY}
-               />
+              />
+
+              <Link href="https://www.figma.com/proto/e1VmnoQvT9wQDNCXyTAoAk/Beauty-by-Chanelle?node-id=0-1&node-type=canvas&viewport=1351%2C-1426%2C0.27&t=B4O2DvcVb5k3ZBZi-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=223%3A152&show-proto-sidebar=1" target="_blank">Link to mobile 📱 prototype.</Link>
+              <br />
+              <br />
+
+              <Link href="https://www.figma.com/proto/e1VmnoQvT9wQDNCXyTAoAk/Beauty-by-Chanelle?node-id=0-1&node-type=canvas&viewport=1351%2C-1426%2C0.27&t=B4O2DvcVb5k3ZBZi-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=304%3A527&show-proto-sidebar=1" target="_blank">Link to desktop 💻 prototype.</Link>
+
+
+              </chanelle>
 
 
 
 
-
-
-
+<Link href="https://www.figma.com/proto/tYvFzMtJQ7Mg36L9ZmXJAV/Naphill-Tennis-Club?node-id=0-1&node-type=canvas&viewport=481%2C359%2C0.03&t=mCa1Fbg0nsFraa56-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=207%3A349&show-proto-sidebar=1" target="_blank" className={linkToPrototypesStyle}>
 <Image
                  src="/Media/Images/Portfolio/UI Design/Naphill-Desktop-Home-page.jpg"
                  width={500}
@@ -159,9 +251,13 @@ From concept to completion, my work is driven by creativity, collaboration, and 
                  alt=""
                  layout="responsive"
                  className={imageMarginY}
-                  />
+                 />
+                 <p>{desktopPrototypeText}</p>
+                 </Link>
 
 
+
+<Link href="https://www.figma.com/proto/tYvFzMtJQ7Mg36L9ZmXJAV/Naphill-Tennis-Club?node-id=0-1&node-type=canvas&viewport=481%2C359%2C0.03&t=mCa1Fbg0nsFraa56-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=207%3A191&show-proto-sidebar=1" target="_blank" className={linkToPrototypesStyle}>
 <Image
                             src="/Media/Images/Portfolio/UI Design/Naphill Tennis Club Mobiel Screens.jpg"
                             width={500}
@@ -169,8 +265,11 @@ From concept to completion, my work is driven by creativity, collaboration, and 
                             alt=""
                             layout="responsive"
                             className={imageMarginY}
-                             />
+                            />
 
+                            <p>{mobilePrototypeText}</p>
+
+                            </Link>
 
                 <Image
                 src="/Media/Images/Portfolio/UI Design/Naphill-Desktop-contact-us.jpg"
@@ -193,7 +292,7 @@ From concept to completion, my work is driven by creativity, collaboration, and 
 
 
 
-
+<Link href="https://www.figma.com/proto/MmmDVxYW4T9L5TNRozYH62/CSSC-Centenary?node-id=0-1&node-type=canvas&viewport=-3%2C68%2C0.15&t=v0pajn9PQOmTNVeq-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=11%3A4&show-proto-sidebar=1" target="_blank" className={linkToPrototypesStyle}>
 
 <Image
                 src="/Media/Images/Portfolio/UI Design/CSSC2.jpg"
@@ -202,9 +301,17 @@ From concept to completion, my work is driven by creativity, collaboration, and 
                 alt=""
                 layout="responsive"
                 className={imageMarginY}
-                 />
+                />
+
+                <p>{desktopPrototypeText}</p>
+
+                </Link>
 
 
+
+
+
+<Link href="https://www.figma.com/proto/MmmDVxYW4T9L5TNRozYH62/CSSC-Centenary?node-id=0-1&node-type=canvas&viewport=-3%2C68%2C0.15&t=v0pajn9PQOmTNVeq-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=28%3A346&show-proto-sidebar=1" target="_blank" className="p-5 my-5 rounded-xl bg-red-200 text-center w-full flex flex-col items-center">
 <div className="w-1/3">
                <Image
                src="/Media/Images/Portfolio/UI Design/CSSC1.jpg"
@@ -216,6 +323,9 @@ From concept to completion, my work is driven by creativity, collaboration, and 
                />
                </div>
 
+               <p>{mobilePrototypeText}</p>
+
+               </Link>
 
 
 
