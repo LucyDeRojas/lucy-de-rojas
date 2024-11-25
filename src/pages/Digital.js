f22a789d5c92f2ad756ca4774f1691cba7975a38
Lucy de Rojas <lucyderojas@gmail.com>
2024-11-25 00:24:52 +0000

    digital design page finshed

--- src/pages/Digital.js
+++ src/pages/Digital.js
@@ -8,9 +8,19 @@ import Layout from "@/Components/Layout";
 
 export default function Digital () {
 
+
+
+        let imageStyling = "my-7 flex-1 drop-shadow-2xl rounded-md";
+
+
+
+
+
+
+
 return (<Layout >
 <h1>Digital Design</h1>
-<main className="px-14">
+<main className="p-14">
 
 <p>
 
@@ -22,6 +32,395 @@ Explore my work to see how creativity meets strategy in projects that drive resu
 
 
 </p>
+
+
+<h2 className="bg-green-400 py-5 text-center my-7 rounded-full col-span-1 md:col-span-2 lg:col-span-3">Animated Gifs:</h2>
+
+
+
+
+{/* images wrapper: */}
+<div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-0 md:gap-10 lg:gap-20 items-center">
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/AnimatedGifs/F1TrySomething.gif"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling} />
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/AnimatedGifs/ScannAppealLogo.gif"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/AnimatedGifs/Generic-e-footer-January-2018-3-forNewsletters.gif"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/AnimatedGifs/MySavings.gif"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+ <Image
+ src="/Media/Images/Portfolio/Digital Design/AnimatedGifs/MySavings+-October-2017.gif"
+ width={500}
+ height={500}
+ alt=""
+ layout="responsive"
+ className={imageStyling}
+  />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/AnimatedGifs/CSSC-Online-March-2017.gif"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+
+
+
+
+<h2 className="bg-green-400 py-5 text-center my-7 rounded-full  col-span-1 md:col-span-2 lg:col-span-3">Static Designs:</h2>
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/Email-Signature-Image-CSSC-Awards-2016.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+ <Image
+ src="/Media/Images/Portfolio/Digital Design/englishHeritage.png"
+ width={500}
+ height={500}
+ alt=""
+ layout="responsive"
+ className={imageStyling}
+  />
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/Facebook-Banner-My-Savings-Plus-launch-2016.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+
+
+ <Image
+ src="/Media/Images/Portfolio/Digital Design/fullEmail.jpg"
+ width={500}
+ height={500}
+ alt=""
+ layout="responsive"
+ className={imageStyling}
+  />
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/gamesInvite2017.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/historicalPalaces.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+ <Image
+ src="/Media/Images/Portfolio/Digital Design/memberGetMember.png"
+ width={500}
+ height={500}
+ alt=""
+ layout="responsive"
+ className={imageStyling}
+  />
+
+
+
+
+  <Image
+  src="/Media/Images/Portfolio/Digital Design/memberGetMember2.png"
+  width={500}
+  height={500}
+  alt=""
+  layout="responsive"
+  className={imageStyling}
+   />
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/memberGetMember3.png"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/meritAward2017.png"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+ <Image
+ src="/Media/Images/Portfolio/Digital Design/meritAwards2016.jpg"
+ width={500}
+ height={500}
+ alt=""
+ layout="responsive"
+ className={imageStyling}
+  />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/merlinSavings.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/MyActive.png"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/onlineShop.png"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/onlineShopXmas.png"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/onlineShopXmas2.png"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/Sarahs-email-images-January-2016.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/Sarahs-email-images-January-20162.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/Sarahs-email-images-January-20163.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/SeasonGreetings.png"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/SwitchAndSave.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/tasteCard.png"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Digital Design/volunteerWeek.png"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+className={imageStyling}
+ />
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </main>
 
