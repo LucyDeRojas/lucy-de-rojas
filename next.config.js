174bdfca1332df664b1de5b5d58fa8642b8a43a9
Lucy de Rojas <lucyderojas@gmail.com>
2024-11-25 01:48:32 +0000

    videos added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.(mp4)$/,
+  //     use: {
+  //       loader: 'file-loader',
+  //       options: {
+  //         name: 'static/media/public/Media/Images/Portfolio/Video/*.mp4',
+  //         publicPath: '/_next/',
+  //       }
+  //     }
+  //   });
+  //   return config;
+  // }
+
+
 }
 
 module.exports = nextConfig
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -10,6 +10,9 @@ import Layout from "@/Components/Layout";
 
 export default function Video () {
 
+
+        let videoItem = "w-full md:w-1/2 xl:w-1/3 rounded-lg drop-shadow-xl";
+
 return (<Layout>
 <h1>Video Editing & <br />Motion Graphics</h1>
 
@@ -26,15 +29,103 @@ Welcome to my video editing and motion graphics portfolio! With a passion for st
 From sleek transitions and animated infographics to polished promotional videos and social media clips, my work combines creativity with technical precision. Whether collaborating on a brand campaign, crafting explainer videos, or enhancing digital experiences, I bring concepts to life through visually compelling and cohesive designs.
 <br /><br />
 Dive in and explore how I transform ideas into motion!
-
-
-
-
 </p>
 
+
+
+
+
+
+{/* videos wrapper: */}
+<div className="flex flex-col w-full place-items-center gap-10 my-7 py-5">
+
+
+
+
+
+<video src="/Media/Images/Portfolio/Video/ScannAppeal Christmas Carols 2023.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+
+<video src="/Media/Images/Portfolio/Video/Elite movie night 2023 FINAL-OutdoorCinema2.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+
+
+
+
+<video src="/Media/Images/Portfolio/Video/Kophill  Climb.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+<video src="/Media/Images/Portfolio/Video/Mario.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+<video src="/Media/Images/Portfolio/Video/What Is Graphic Design.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+<video src="/Media/Images/Portfolio/Video/CSSC Star Wars Centenary Introduction original music.mp4" type="video/mp4"controls className={videoItem}></video>
+
+
+
+
+
+<video src="/Media/Images/Portfolio/Video/GoKartingTaplow.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+
+
+
+<video src="/Media/Images/Portfolio/Video/StephieFame.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+<video src="/Media/Images/Portfolio/Video/InviteToTheGames.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+<video src="/Media/Images/Portfolio/Video/LewisCup.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+
+
+
+
+<video src="/Media/Images/Portfolio/Video/ThankYouVolunteers.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+
+
+
+
+
+
+
+<video src="/Media/Images/Portfolio/Video/Naphill Tennis Club - Tennis Ball Machine.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+<video src="/Media/Images/Portfolio/Video/NaphillTennisClub - SlowMotion.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+
+<video src="/Media/Images/Portfolio/Video/What Is Tennis.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+<video src="/Media/Images/Portfolio/Video/Vandalism.mp4" width="100%" height="auto" type="video/mp4"controls className={videoItem}></video>
+
+
+
+
+</div>
 </main>
-
-
 </Layout>)};
 
 
