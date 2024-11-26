9ae8cdd2ad3a8ef14a218b4ae739e5cc63ef645b
Lucy de Rojas <lucyderojas@gmail.com>
2024-11-26 23:05:16 +0000

    addeed content to print page

--- src/pages/Print.js
+++ src/pages/Print.js
@@ -9,6 +9,15 @@ import Layout from "@/Components/Layout";
 
 export default function Print () {
 
+
+        let imagesALLWrapper = "w-full flex flex-col bg-red-300 justify-center my-5 py-5 items-center";
+        let imagesWrapper = "w-1/2 flex flex-col bg-green-500 p-5 rounded-2xl gap-5 drop-shadow-2xl my-5";
+
+
+
+
+
+
 return (<Layout>
         <Head>
                 <title>Lucy de Rojas - Print Design</title>
@@ -29,10 +38,318 @@ Thank you for taking the time to explore my work—let’s create something extr
 </p>
 
 
-</main>
 
 
-</Layout>)};
+{/* all images wrapper: */}
+<div className={imagesALLWrapper}>
+
+
+<div className={imagesWrapper}>
+<Image
+src="/Media/Images/Portfolio/Print Design/Harveys/NU-Solutions-Sustainable-Solutions.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+/>
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Harveys/Grains.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+/>
+
+</div>
+
+
+
+
+
+
+
+<div className={imagesWrapper}>
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneAutumn2019/Lesiure_Scene_Autumn_Winter_2019-1.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneAutumn2019/LesiureSceneAutumn2019---5.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+</div>
+
+
+
+
+
+
+
+<div className={imagesWrapper}>
+
+        <Image
+        src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneSpring2016/Lesiure_Scene_Spring_Summer_2016.jpg"
+        width={500}
+        height={500}
+        alt=""
+        layout="responsive"
+         />
+
+
+         <Image
+         src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneSpring2016/Lesiure_Scene_Spring_Summer_20168.jpg"
+         width={500}
+         height={500}
+         alt=""
+         layout="responsive"
+          />
+</div>
+
+
+
+
+
+
+
+
+<div className={imagesWrapper}>
+
+
+        <Image
+        src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneAutumn2017/Lesiure_Scene_Spring_Summer_2017.jpg"
+        width={500}
+        height={500}
+        alt=""
+        layout="responsive"
+         />
+
+
+
+
+         <Image
+         src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneAutumn2017/LesiureSceneSpring20175.jpg"
+         width={500}
+         height={500}
+         alt=""
+         layout="responsive"
+          />
+</div>
+
+
+
+
+
+
+
+
+<div className={imagesWrapper}>
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneAutumn2017/Lesiure_Scene_Autumn_Winter_2018.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneAutumn2017/LeisureSceneAutumn2017--6.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<div className={imagesWrapper}>
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Booklets/BenefitsOfMembership/CSSCBenefitsofMembership---.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Booklets/BenefitsOfMembership/CSSCBenefitsofMembership---16.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<div className={imagesWrapper}>
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Booklets/WhatsOnInLondon2017/WhatsOnInLondon2017-----.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Booklets/WhatsOnInLondon2017/WhatsOnInLondon2017-----14.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+</div>
+
+
+
+
+
+
+
+<div className={imagesWrapper}>
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Booklets/BenefitsOfMembership2017/CSSCBenefitsofMembership2017---.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Booklets/BenefitsOfMembership2017/CSSCBenefitsofMembership2017---5.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+<div className={imagesWrapper}>
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneSpring2018/Lesiure_Scene_Spring_Summer_2018.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Leisrure_Scenes/SPREADS/LeisureSceneSpring2018/LesiureSceneSpring2018---9.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+</div>
+
+
+
+
+
+
+
+<div className={imagesWrapper}>
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Booklets/WhatsOnInLindon2016/WhatsOnInLondon2016---.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+
+
+<Image
+src="/Media/Images/Portfolio/Print Design/Booklets/WhatsOnInLindon2016/WhatsOnInLondon2016---2.jpg"
+width={500}
+height={500}
+alt=""
+layout="responsive"
+ />
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+</div>   {/* end of all images wrapper */}
+</main></Layout>)};
 
 
 
